0ececee9ed5fa7b236acc0d74ff47cc354af12a4
Chris Allen <Chris@zipcodes-MacBook-Pro-350.local>
2021-04-05 22:20:35 -0400

    Made an attempt at getByID'

--- src/main/java/daos/ConcreteDAO.java
+++ src/main/java/daos/ConcreteDAO.java
@@ -1,10 +1,33 @@
 package daos;
 
+import java.sql.*;
 import java.util.List;
 
 public class ConcreteDAO<T> implements DAO<T>{
+    String dbUrl;
+    String username;
+    String password;
+    Connection connection = null;
+
+    public ConcreteDAO(String dbURL,String username, String password){
+        this.dbUrl = dbURL;
+        this.username = username;
+        this.password = password;
+    }
 
     public T findById(int id) {
+
+        try{
+            connection = DriverManager.getConnection(dbUrl, username, password);
+            PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM car WHERE id = ?");
+            pstmt.setInt(1,id);
+            ResultSet rs = pstmt.executeQuery();
+            return (T) rs; //NO IDEA IF THIS IS CORRECT LOL
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
         return null;
     }
 
